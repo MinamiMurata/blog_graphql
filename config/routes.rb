dca5463a1c874ac0b65fcf3353876815539101c4
minami murata <minami_murata@outlook.com>
2023-06-20 23:15:26 +0900

    install graphql

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :blogs
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
+end
